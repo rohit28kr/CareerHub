b0df16700c59ee2e4123676608e63a13283522ff
Rohit Kumar <rohit20082004@gmail.com>
2026-06-01 01:07:25 +0530

    Added Render deployment configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn carrerhub.wsgi:application
--- carrerhub/settings.py
+++ carrerhub/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j=4d$t-8ck9sh5#dv*$1o%!iux1xw-)g3fb()h3jqx!br7*v5%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
