e90d2e25df41d257bf386d4746f43a6acaeaee07
Rohit Kumar <rohit20082004@gmail.com>
2026-06-01 01:54:02 +0530

    upadate eamil environment variavle

--- carrerhub/settings.py
+++ carrerhub/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'carrerhub.com@gmail.com'  
-EMAIL_HOST_PASSWORD = 'qgpr xywc dczs oysw'  
+EMAIL_HOST_USER = 'carrerhub.co@gmail.com'  
+EMAIL_HOST_PASSWORD = 'wtxz cqpj hggd dgzb'  
